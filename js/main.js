271ee5c21a728c0926a0faa613644d8dc45fe3d5
branchaw <branchaw@gmail.com>
2016-02-19 22:25:56 -0500

    completed basic requirements.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,87 @@
 /* Custom JS goes here. */
 
-// For this assignment you'll need to do a few things:
-// 1. Create a document ready handler.
-// 2. Define a validation object for use on your page.
-// 3. Connect the validation object to an event handler tied to the submit button.
+// Document ready handler
+$(document).on('ready', function(){
+  
+// Validation object
+  $('#order-form').validate({
+  
+    // Event handler tied to the submit button.
+      submitHandler: function(form){
+      form.submit(); 
+    },
+    
+    rules: {
+      "your-name": {
+        required: true,
+        lettersonly: true,
+        maxlength: 128,
+        minlength: 2
+      },
+      "your-address": {
+        required: true
+      },
+      "your-city": {
+        required: true
+      },
+      "your-state": {
+        required: true,
+        maxlength: 2,
+        minlength: 2,
+        lettersonly: true
+        //TODO: stretch check state abbrev
+      },
+      "your-zip": {
+        required: true,
+        digits: true,
+        maxlength: 5,
+        minlength: 5
+      },
+      "card-holder-name": {
+        required: true,
+        maxlength: 128,
+        lettersonly: true        
+      },
+      "card-number": {
+        required: true,
+        creditcard: true
+      },
+      "expiry-month": {
+        required: true
+      },
+      "cvv": {
+        required: true,
+        maxlength: 3,
+        minlength: 3,
+        digits: true
+      },
+      "shipping-method": {
+        required: true
+      },
+       "comments": {
+          required: false,
+          maxlength: 500
+        }
+      },    
+      messages: {
+        "your-name": "Please specify your name in letters only.",
+        "your-address": "Enter your street address.",
+        "your-city": "Enter your city name.",
+        "your-state": "Enter your state's 2-letter abbreviation.",
+        "your-zip": "Enter your 5-digit zip code.",
+        "card-holder-name": "Please enter the credit card holder name as it appears on the card.",
+        "card-number": "Enter your 16-digit credit card number.",
+        "expiry-month": "Enter the expiration month and year.",
+        "cvv": "Enter the three digit code on the back of your card.",
+        "shipping-method": "Please select a shipping method."
+      }
+    
+  });
+  
+ $.validator.addMethod( "lettersonly", function( value, element ) {
+  return this.optional( element ) || /^[a-z]+$/i.test( value );
+ }, "Letters only please" );
+  
+}); 
 
-// Refer to the `index.html` file for the validation rules that must be enforced.
+
